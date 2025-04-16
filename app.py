eca2d2cdee879a226499dcd266f472b85d743288
mdirtizahossain1999 <mdirtiza.hossain1999@gmail.com>
2025-04-16 13:28:58 +0600

    db fix 2

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 
 # Database Configuration
 base_dir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'database.db')
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "ecommerce_secret"
 
 
